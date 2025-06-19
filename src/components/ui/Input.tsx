6ded7fddfe3a1146062a496be97638ae9cb4cdce
Lisabolingers <rdmcollectiontee@gmail.com>
2025-06-19 23:52:44 +0300

    Fix Input component export to resolve import errors

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -4,7 +4,7 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string;
 }
 
-const Input: React.FC<InputProps> = ({ className = '', ...props }) => {
+export const Input: React.FC<InputProps> = ({ className = '', ...props }) => {
   return (
     <input
       className={`px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-700 text-gray-900 dark:text-white placeholder-gray-400 focus:ring-2 focus:ring-orange-500 focus:border-transparent ${className}`}
@@ -12,5 +12,3 @@ const Input: React.FC<InputProps> = ({ className = '', ...props }) => {
     />
   );
 };
-
-export default Input;
