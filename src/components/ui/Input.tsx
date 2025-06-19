8c99bc7c2486ac0af48e34135cb48a55d3cb3e92
Lisabolingers <rdmcollectiontee@gmail.com>
2025-06-20 00:44:24 +0300

    Fix Input component export

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -4,7 +4,7 @@ interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
   className?: string;
 }
 
-export const Input: React.FC<InputProps> = ({ className = '', ...props }) => {
+const Input: React.FC<InputProps> = ({ className = '', ...props }) => {
   return (
     <input
       className={`px-3 py-2 border border-gray-300 dark:border-gray-600 rounded-lg bg-white dark:bg-gray-700 text-gray-900 dark:text-white placeholder-gray-400 focus:ring-2 focus:ring-orange-500 focus:border-transparent ${className}`}
@@ -12,3 +12,6 @@ export const Input: React.FC<InputProps> = ({ className = '', ...props }) => {
     />
   );
 };
+
+export { Input };
+export default Input;
